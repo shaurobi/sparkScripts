abb8db0ecdc89a8a294922a92401656bdc49eaf7
shaurobi <shaurobi@cisco.com>
2016-09-23 07:52:48 +1000

    written updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sparkScripts
+Hi there!
+
+I'm currently building a number of scripts to help with everyday functions in Spark. 
--- roomExport.py
+++ roomExport.py
@@ -1,48 +1,50 @@
 # imports list of users in room, imports into array, then dumps into team. At this stage, team will be predefined
 import json
 import requests
-
-#get list of users in room
-
-
 #define authentication key
 key = "Bearer MGE4NTYzYTMtZmYzNi00Mzk4LTkyMGEtODgzZmNhYmVhYjJkZGRlYzI4NjctYjI3"
+
+
+#This section -- get list of users in room
+
 #define URL we're trying to hit
 url = "https://api.ciscospark.com/v1/memberships"
-#build the query
+#build the query, specifying the room you want to hit
 querystring = {"roomId":"Y2lzY29zcGFyazovL3VzL1JPT00vNjNkZTE0MDAtODBhMC0xMWU2LTkzYjMtOTVhMjYxMjNlYTkx"}
-#don't forget the headers
+#don't forget the headers, and use that authorisation we used earlier, american spelling tho
 headers = {
-    'authorization': "Bearer MGE4NTYzYTMtZmYzNi00Mzk4LTkyMGEtODgzZmNhYmVhYjJkZGRlYzI4NjctYjI3",
+    'authorization': key,
     'cache-control': "no-cache",
     'postman-token': "69a83b1c-b337-8e43-7f7b-5282386e3148"
     }
-#capture the response
+#send it using REST, capture the response
 response = requests.request("GET", url, headers=headers, params=querystring)
-
 #load the response into a dictionary, using Python's JSON parser
 room_dict = json.loads(response.text)
-
 #create a list to drop people's emails into
 list = []
 #drop the emails into a list
 for room in room_dict['items']:
     list.append(room['personEmail'])
-#EVERYTHING IS WORKING UP TO THIS POINT, HERE THERE BE DRAGONS
+    
+#now that we have the emails, lets put them into another room
+
+#define URL we need to hit
 url2 = "https://api.ciscospark.com/v1/team/memberships"
+#build the headers again, not sure why we're using cache-control or postman-token, postman added them in
 headers = {
-	    'authorization': "Bearer MGE4NTYzYTMtZmYzNi00Mzk4LTkyMGEtODgzZmNhYmVhYjJkZGRlYzI4NjctYjI3",
+	    'authorization': key,
 	    'content-type': "application/json",
 	    'cache-control': "no-cache",
 	    'postman-token': "8499bb62-c358-b25e-2ad0-315be5cd80b2"
 	    }
-
+#now we've built the headers, lets loop through the emails in the list
 for people in list:
-	data = {
+	data = {#building the payload here, next line specifies the team youre migrating to
 		'teamId':   'Y2lzY29zcGFyazovL3VzL1RFQU0vMGY2ZGU4OTAtODBhMS0xMWU2LWFiNjAtMTEyMTljNzY4ZDc5',
-	   	'personEmail': people
+	   	'personEmail': people #build the email into the payload
 	   	}
-	payload = json.dumps(data)
-	print payload
-	response = requests.request("POST", url2, data=payload, headers=headers)
-	print response.status_code
+	payload = json.dumps(data) #build the payload into JSON
+	print payload #prints, just for validation
+	response = requests.request("POST", url2, data=payload, headers=headers) #submits the request
+	print response.status_code #prints a status code after every payload, 200 is good!
